6c02bb80cc2e39a4f672380c7fbc5dfb9c10c3d5
Rusln <cpptypxe@ya.ru>
2025-03-26 10:25:20 -0700

    Initial Commit

---
+++ Crypto/CryptoApp.swift
@@ -0,0 +1,17 @@
+//
+//  CryptoApp.swift
+//  Crypto
+//
+//  Created by dofxmine on 26.03.2025.
+//
+
+import SwiftUI
+
+@main
+struct CryptoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
