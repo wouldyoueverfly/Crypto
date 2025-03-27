9df6548f6c4285360f7cf453c8bd60b74473f795
Rusln <cpptypxe@ya.ru>
2025-03-27 09:38:03 -0700

    [Feature] add coins list

---
+++ Crypto/Core/Home/ViewModels/HomeViewModel.swift
@@ -0,0 +1,21 @@
+//
+//  HomeViewModel.swift
+//  Crypto
+//
+//  Created by dofxmine on 27.03.2025.
+//
+
+import Foundation
+
+class HomeViewModel: ObservableObject {
+    
+    @Published var allCoins: [CoinModel] = []
+    @Published var portfolioCoins: [CoinModel] = []
+    
+    init() {
+        DispatchQueue.main.asyncAfter(deadline: .now() + 2.0) {
+            self.allCoins.append(DeveloperPreview.instance.coin)
+            self.portfolioCoins.append(DeveloperPreview.instance.coin)
+        }
+    }
+}
--- Crypto/Core/Home/Views/HomeView.swift
+++ Crypto/Core/Home/Views/HomeView.swift
@@ -7,8 +7,10 @@
 
 import SwiftUI
 
+
 struct HomeView: View {
     
+    @EnvironmentObject private var vm: HomeViewModel
     @State var showPortfolio: Bool = false
     
     var body: some View {
@@ -20,19 +22,30 @@ struct HomeView: View {
             // content layer
             VStack {
                 homeHeader
+                columnTitles
+                if !showPortfolio {
+                    allCoinsList
+                    .transition(.move(edge: .leading))
+                }
+                if showPortfolio {
+                    portfolioCoinsList
+                        .transition(.move(edge: .trailing))
+                }
                 Spacer(minLength: 0)
             }
         }
     }
 }
 
-#Preview {
-    NavigationView {
-        HomeView()
-            .toolbar(.hidden)
+struct HomeView_Previews: PreviewProvider {
+    static var previews: some View {
+        NavigationView {
+            HomeView()
+                .toolbar(.hidden)
+        }
+        .environmentObject(dev.homeVM)
     }
 }
-
 
 extension HomeView {
     private var homeHeader: some View {
@@ -57,6 +70,40 @@ extension HomeView {
                     }
                 }
         }
+        .padding(.horizontal)
+    }
+    
+    private var allCoinsList: some View {
+        List {
+            ForEach(vm.allCoins) { coin in
+                CoinRowView(coin: coin, showHoldingsColumn: false)
+                    .listRowInsets(.init(top: 10, leading: 0, bottom: 10, trailing: 10))
+            }
+        }
+        .listStyle(PlainListStyle())
+    }
+    
+    private var portfolioCoinsList: some View {
+        List {
+            ForEach(vm.portfolioCoins) { coin in
+                CoinRowView(coin: coin, showHoldingsColumn: true)
+                    .listRowInsets(.init(top: 10, leading: 0, bottom: 10, trailing: 10))
+            }
+        }
+        .listStyle(PlainListStyle())
+    }
+    private var columnTitles: some View {
+        HStack {
+            Text("Coin")
+            Spacer()
+            if showPortfolio {
+                Text("Holdings")
+            }
+            Text("Price")
+                .frame(width: UIScreen.main.bounds.width / 3.5, alignment: .trailing)
+        }
+        .font(.caption)
+        .foregroundStyle(.secondaryText)
         .padding(.horizontal)
     }
 }
